34e52b53921f73e5873c6702a69b5265d344a5f6
lamjiidii1 <lamjiidii1@gmail.com>
2024-02-11 12:27:29 +0530

    common: Silence OpenGLRenderer log spam
    OpenGLRenderer: Unable to match the desired swap behavior
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- common-prop.mk
+++ common-prop.mk
@@ -131,6 +131,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.lmk.upgrade_pressure=40 \
     ro.lmk.downgrade_pressure=60
 
+# Log
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.log.tag.OpenGLRenderer=S
+
 # Kernel
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.kernel_version=$(TARGET_KERNEL_VERSION)
